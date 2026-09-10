1a98e6e9c4d49d99301c833d8a1d8d3882e57ea8
zuohuaijun <zuohuaijun@163.com>
2026-09-10 13:05:09 +0000

    update Admin.NET/Admin.NET.Core/Entity/SysJobTrigger.cs.
    Signed-off-by: zuohuaijun <zuohuaijun@163.com>

--- Admin.NET/Admin.NET.Core/Entity/SysJobTrigger.cs
+++ Admin.NET/Admin.NET.Core/Entity/SysJobTrigger.cs
@@ -1,4 +1,4 @@
-﻿// Admin.NET 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+// Admin.NET 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
 //
 // 本项目主要遵循 MIT 许可证和 Apache 许可证（版本 2.0）进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 和 LICENSE-APACHE 文件。
 //
@@ -39,7 +39,7 @@ public partial class SysJobTrigger : EntityBaseId
     /// </summary>
     [SugarColumn(ColumnDescription = "程序集", Length = 128)]
     [MaxLength(128)]
-    public string? AssemblyName { get; set; } = "Furion.Pure";
+    public string? AssemblyName { get; set; } = "Furion";
 
     /// <summary>
     /// 参数
